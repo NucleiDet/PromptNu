1cbebde9b65a5ce9da808ea113c7a604bd252066
Nuclei-related tasks <153095467+NucleiDet@users.noreply.github.com>
2025-03-27 01:17:59 +0800

    Add files via upload

---
+++ tiseg/utils/f1_metric.py
@@ -0,0 +1,428 @@
+from collections import OrderedDict
+import os
+
+import mmcv
+import numpy as np
+import torch
+
+import cv2
+import numpy as np
+import scipy
+from scipy.optimize import linear_sum_assignment
+import scipy.io as sio
+
+from scipy.ndimage import filters, measurements
+from scipy.ndimage.morphology import (
+    binary_dilation,
+    binary_fill_holes,
+    distance_transform_cdt,
+    distance_transform_edt,
+)
+
+from skimage.segmentation import watershed
+from .misc import get_bounding_box, remove_small_objects
+
+import warnings
+
+
+def noop(*args, **kargs):
+    pass
+
+
+warnings.warn = noop
+
+
+####
+def __proc_np_hv(pred):
+    """Process Nuclei Prediction with XY Coordinate Map.
+
+    Args:
+        pred: prediction output, assuming 
+              channel 0 contain probability map of nuclei
+              channel 1 containing the regressed X-map
+              channel 2 containing the regressed Y-map
+
+    """
+    pred = np.array(pred, dtype=np.float32)
+
+    blb_raw = pred[..., 0]
+    h_dir_raw = pred[..., 1]
+    v_dir_raw = pred[..., 2]
+
+    # processing
+    blb = np.array(blb_raw >= 0.5, dtype=np.int32)
+
+    blb = measurements.label(blb)[0]
+    blb = remove_small_objects(blb, min_size=10)
+    blb[blb > 0] = 1  # background is 0 already
+
+    h_dir = cv2.normalize(
+        h_dir_raw, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F
+    )
+    v_dir = cv2.normalize(
+        v_dir_raw, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F
+    )
+
+    sobelh = cv2.Sobel(h_dir, cv2.CV_64F, 1, 0, ksize=21)
+    sobelv = cv2.Sobel(v_dir, cv2.CV_64F, 0, 1, ksize=21)
+
+    sobelh = 1 - (
+        cv2.normalize(
+            sobelh, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F
+        )
+    )
+    sobelv = 1 - (
+        cv2.normalize(
+            sobelv, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F
+        )
+    )
+
+    overall = np.maximum(sobelh, sobelv)
+    overall = overall - (1 - blb)
+    overall[overall < 0] = 0
+
+    dist = (1.0 - overall) * blb
+    ## nuclei values form mountains so inverse to get basins
+    dist = -cv2.GaussianBlur(dist, (3, 3), 0)
+
+    overall = np.array(overall >= 0.4, dtype=np.int32)
+
+    marker = blb - overall
+    marker[marker < 0] = 0
+    marker = binary_fill_holes(marker).astype("uint8")
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    marker = cv2.morphologyEx(marker, cv2.MORPH_OPEN, kernel)
+    marker = measurements.label(marker)[0]
+    marker = remove_small_objects(marker, min_size=10)
+
+    proced_pred = watershed(dist, markers=marker, mask=blb)
+
+    return proced_pred
+
+
+####
+def process(pred_map, nr_types=None, return_centroids=False):
+    """Post processing script for image tiles.
+
+    Args:
+        pred_map: commbined output of tp, np and hv branches, in the same order
+        nr_types: number of types considered at output of nc branch
+        overlaid_img: img to overlay the predicted instances upon, `None` means no
+        type_colour (dict) : `None` to use random, else overlay instances of a type to colour in the dict
+        output_dtype: data type of output
+    
+    Returns:
+        pred_inst:     pixel-wise nuclear instance segmentation prediction
+        pred_type_out: pixel-wise nuclear type prediction 
+
+    """
+    if nr_types is not None:
+        pred_type = pred_map[..., :1]
+        pred_inst = pred_map[..., 1:]
+        pred_type = pred_type.astype(np.int32)
+    else:
+        pred_inst = pred_map
+
+    pred_inst = np.squeeze(pred_inst)
+    pred_inst = __proc_np_hv(pred_inst)
+
+    inst_info_dict = None
+    if return_centroids or nr_types is not None:
+        inst_id_list = np.unique(pred_inst)[1:]  # exlcude background
+        inst_info_dict = {}
+        for inst_id in inst_id_list:
+            inst_map = pred_inst == inst_id
+            # TODO: chane format of bbox output
+            rmin, rmax, cmin, cmax = get_bounding_box(inst_map)
+            inst_bbox = np.array([[rmin, cmin], [rmax, cmax]])
+            inst_map = inst_map[
+                inst_bbox[0][0] : inst_bbox[1][0], inst_bbox[0][1] : inst_bbox[1][1]
+            ]
+            inst_map = inst_map.astype(np.uint8)
+            inst_moment = cv2.moments(inst_map)
+            inst_contour = cv2.findContours(
+                inst_map, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+            )
+            # * opencv protocol format may break
+            inst_contour = np.squeeze(inst_contour[0][0].astype("int32"))
+            # < 3 points dont make a contour, so skip, likely artifact too
+            # as the contours obtained via approximation => too small or sthg
+            if inst_contour.shape[0] < 3:
+                continue
+            if len(inst_contour.shape) != 2:
+                continue # ! check for trickery shape
+            inst_centroid = [
+                (inst_moment["m10"] / inst_moment["m00"]),
+                (inst_moment["m01"] / inst_moment["m00"]),
+            ]
+            inst_centroid = np.array(inst_centroid)
+            inst_contour[:, 0] += inst_bbox[0][1]  # X
+            inst_contour[:, 1] += inst_bbox[0][0]  # Y
+            inst_centroid[0] += inst_bbox[0][1]  # X
+            inst_centroid[1] += inst_bbox[0][0]  # Y
+            inst_info_dict[inst_id] = {  # inst_id should start at 1
+                "bbox": inst_bbox,
+                "centroid": inst_centroid,
+                "contour": inst_contour,
+                "type_prob": None,
+                "type": None,
+            }
+
+    if nr_types is not None:
+        #### * Get class of each instance id, stored at index id-1
+        for inst_id in list(inst_info_dict.keys()):
+            rmin, cmin, rmax, cmax = (inst_info_dict[inst_id]["bbox"]).flatten()
+            inst_map_crop = pred_inst[rmin:rmax, cmin:cmax]
+            inst_type_crop = pred_type[rmin:rmax, cmin:cmax]
+            inst_map_crop = (
+                inst_map_crop == inst_id
+            )  # TODO: duplicated operation, may be expensive
+            inst_type = inst_type_crop[inst_map_crop]
+            type_list, type_pixels = np.unique(inst_type, return_counts=True)
+            type_list = list(zip(type_list, type_pixels))
+            type_list = sorted(type_list, key=lambda x: x[1], reverse=True)
+            inst_type = type_list[0][0]
+            if inst_type == 0:  # ! pick the 2nd most dominant if exist
+                if len(type_list) > 1:
+                    inst_type = type_list[1][0]
+            type_dict = {v[0]: v[1] for v in type_list}
+            type_prob = type_dict[inst_type] / (np.sum(inst_map_crop) + 1.0e-6)
+            inst_info_dict[inst_id]["type"] = int(inst_type)
+            inst_info_dict[inst_id]["type_prob"] = float(type_prob)
+
+    # print('here')
+    # ! WARNING: ID MAY NOT BE CONTIGUOUS
+    # inst_id in the dict maps to the same value in the `pred_inst`
+    return pred_inst, inst_info_dict
+
+
+def pair_coordinates(setA, setB, radius):
+    """Use the Munkres or Kuhn-Munkres algorithm to find the most optimal 
+    unique pairing (largest possible match) when pairing points in set B 
+    against points in set A, using distance as cost function.
+
+    Args:
+        setA, setB: np.array (float32) of size Nx2 contains the of XY coordinate
+                    of N different points 
+        radius: valid area around a point in setA to consider 
+                a given coordinate in setB a candidate for match
+    Return:
+        pairing: pairing is an array of indices
+        where point at index pairing[0] in set A paired with point
+        in set B at index pairing[1]
+        unparedA, unpairedB: remaining poitn in set A and set B unpaired
+
+    """
+    # * Euclidean distance as the cost matrix
+    pair_distance = scipy.spatial.distance.cdist(setA, setB, metric='euclidean')
+
+    # * Munkres pairing with scipy library
+    # the algorithm return (row indices, matched column indices)
+    # if there is multiple same cost in a row, index of first occurence 
+    # is return, thus the unique pairing is ensured
+    indicesA, paired_indicesB = linear_sum_assignment(pair_distance)
+
+    # extract the paired cost and remove instances 
+    # outside of designated radius
+    pair_cost = pair_distance[indicesA, paired_indicesB]
+
+    pairedA = indicesA[pair_cost <= radius]
+    pairedB = paired_indicesB[pair_cost <= radius]
+
+    pairing = np.concatenate([pairedA[:,None], pairedB[:,None]], axis=-1)
+    unpairedA = np.delete(np.arange(setA.shape[0]), pairedA)
+    unpairedB = np.delete(np.arange(setB.shape[0]), pairedB)
+    return pairing, unpairedA, unpairedB
+
+
+def _f1_type(paired_true, paired_pred, unpaired_true, unpaired_pred, type_id, w):
+    type_samples = (paired_true == type_id) | (paired_pred == type_id)
+
+    paired_true = paired_true[type_samples]
+    paired_pred = paired_pred[type_samples]
+
+    tp_dt = ((paired_true == type_id) & (paired_pred == type_id)).sum()
+    tn_dt = ((paired_true != type_id) & (paired_pred != type_id)).sum()
+    fp_dt = ((paired_true != type_id) & (paired_pred == type_id)).sum()
+    fn_dt = ((paired_true == type_id) & (paired_pred != type_id)).sum()
+
+    exhaustive=True
+    
+    if not exhaustive:
+        ignore = (paired_true == -1).sum()
+        fp_dt -= ignore
+
+    fp_d = (unpaired_pred == type_id).sum()
+    fn_d = (unpaired_true == type_id).sum()
+
+    # 公式8
+    f1_type = (2 * (tp_dt + tn_dt)) / (
+        2 * (tp_dt + tn_dt)
+        + w[0] * fp_dt
+        + w[1] * fn_dt
+        + w[2] * fp_d
+        + w[3] * fn_d
+    )
+    return f1_type
+
+def run_nuclei_type_stat(inst_info_dict_list, true_dir, flag, type_uid_list=None, exhaustive=True):
+    """GT must be exhaustively annotated for instance location (detection).
+
+    Args:
+        true_dir, pred_dir: Directory contains .mat annotation for each image. 
+                            Each .mat must contain:
+                    --`inst_centroid`: Nx2, contains N instance centroid
+                                       of mass coordinates (X, Y)
+                    --`inst_type`    : Nx1: type of each instance at each index
+                    `inst_centroid` and `inst_type` must be aligned and each
+                    index must be associated to the same instance
+        type_uid_list : list of id for nuclei type which the score should be calculated.
+                        Default to `None` means available nuclei type in GT.
+        exhaustive : Flag to indicate whether GT is exhaustively labelled
+                     for instance types
+                     
+    """
+
+    paired_all = []  # unique matched index pair
+    unpaired_true_all = []  # the index must exist in `true_inst_type_all` and unique
+    unpaired_pred_all = []  # the index must exist in `pred_inst_type_all` and unique
+    true_inst_type_all = []  # each index is 1 independent data point
+    pred_inst_type_all = []  # each index is 1 independent data point
+    for file_idx in range(14):
+        basename = 'test_' + str(file_idx+1)
+
+        true_info = sio.loadmat(os.path.join(true_dir, basename + ".mat"))
+        # dont squeeze, may be 1 instance exist
+        true_centroid = (true_info["inst_centroid"]).astype("float32") # [865, 2]
+        true_inst_type = (true_info["inst_type"]).astype("int32") #[865, 1], 第一个是细胞核数量
+
+        if true_centroid.shape[0] != 0:
+            true_inst_type = true_inst_type[:, 0]
+        else:  # no instance at all
+            true_centroid = np.array([[0, 0]])
+            true_inst_type = np.array([0])
+
+        pred_centroid  = []
+        pred_inst_type = []
+        inst_info_dict = inst_info_dict_list[file_idx]
+
+        for _, inst_info in inst_info_dict.items():
+            pred_centroid.append(inst_info["centroid"])
+            pred_inst_type.append(inst_info["type"])
+
+
+        # dont squeeze, may be 1 instance exist
+        pred_centroid = np.asarray(pred_centroid).astype("float32") # [865, 2]
+        pred_inst_type = np.asarray(pred_inst_type)[:, np.newaxis].astype("int32") #[865, 1]
+
+        if pred_centroid.shape[0] != 0:
+            pred_inst_type = pred_inst_type[:, 0]
+        else:  # no instance at all
+            pred_centroid = np.array([[0, 0]])
+            pred_inst_type = np.array([0])
+
+        # * for converting the GT type in CoNSeP
+        # 这里可以合并类别
+        if flag == 4:
+            true_inst_type[(true_inst_type == 3) | (true_inst_type == 4)] = 3
+            true_inst_type[(true_inst_type == 5) | (true_inst_type == 6) | (true_inst_type == 7)] = 4
+            pred_inst_type[(pred_inst_type == 3) | (pred_inst_type == 4)] = 3
+            pred_inst_type[(pred_inst_type == 5) | (pred_inst_type == 6) | (pred_inst_type == 7)] = 4
+
+        # ! if take longer than 1min for 1000 vs 1000 pairing, sthg is wrong with coord
+        paired, unpaired_true, unpaired_pred = pair_coordinates(
+            true_centroid, pred_centroid, 12
+        )#[865,2],null, null
+
+        # * Aggreate information
+        # get the offset as each index represent 1 independent instance
+        true_idx_offset = (
+            true_idx_offset + true_inst_type_all[-1].shape[0] if file_idx != 0 else 0
+        )
+        pred_idx_offset = (
+            pred_idx_offset + pred_inst_type_all[-1].shape[0] if file_idx != 0 else 0
+        )
+        true_inst_type_all.append(true_inst_type)
+        pred_inst_type_all.append(pred_inst_type)
+
+        # increment the pairing index statistic
+        if paired.shape[0] != 0:  # ! sanity
+            paired[:, 0] += true_idx_offset
+            paired[:, 1] += pred_idx_offset
+            paired_all.append(paired)
+
+        unpaired_true += true_idx_offset
+        unpaired_pred += pred_idx_offset
+        unpaired_true_all.append(unpaired_true)
+        unpaired_pred_all.append(unpaired_pred)
+
+    paired_all = np.concatenate(paired_all, axis=0)
+    unpaired_true_all = np.concatenate(unpaired_true_all, axis=0)
+    unpaired_pred_all = np.concatenate(unpaired_pred_all, axis=0)
+    true_inst_type_all = np.concatenate(true_inst_type_all, axis=0)
+    pred_inst_type_all = np.concatenate(pred_inst_type_all, axis=0)
+
+    paired_true_type = true_inst_type_all[paired_all[:, 0]]
+    paired_pred_type = pred_inst_type_all[paired_all[:, 1]]
+    unpaired_true_type = true_inst_type_all[unpaired_true_all]
+    unpaired_pred_type = pred_inst_type_all[unpaired_pred_all]
+
+    # overall
+    # * quite meaningless for not exhaustive annotated dataset
+    w = [1, 1]
+    tp_d = paired_pred_type.shape[0]
+    fp_d = unpaired_pred_type.shape[0]
+    fn_d = unpaired_true_type.shape[0]
+
+    tp_tn_dt = (paired_pred_type == paired_true_type).sum()
+    fp_fn_dt = (paired_pred_type != paired_true_type).sum()
+
+    if not exhaustive:
+        ignore = (paired_true_type == -1).sum()
+        fp_fn_dt -= ignore
+
+    acc_type = tp_tn_dt / (tp_tn_dt + fp_fn_dt)
+    f1_d = 2 * tp_d / (2 * tp_d + w[0] * fp_d + w[1] * fn_d)
+
+    w = [2, 2, 1, 1]
+
+    if type_uid_list is None:
+        type_uid_list = np.unique(true_inst_type_all).tolist()
+
+    results_list = [f1_d, acc_type]
+    for type_uid in type_uid_list:
+        f1_type = _f1_type(
+            paired_true_type,
+            paired_pred_type,
+            unpaired_true_type,
+            unpaired_pred_type,
+            type_uid,
+            w,
+        )
+        results_list.append(f1_type)
+
+    np.set_printoptions(formatter={"float": "{: 0.5f}".format})
+    return results_list
+
+
+def pre_eval_to_F1(pre_eval_results, names, nan_to_num=None):
+    #注意在这里需要修改类别数
+    inst_info_dict_list = [i for i in range(1, 15)]
+    post_proc_kwargs = {'nr_types': 8, 'return_centroids': True} #nr_type包含背景
+
+    for idx, name in enumerate(names):
+        pred_inst, inst_info_dict = process(np.squeeze(pre_eval_results[idx]), **post_proc_kwargs)
+        inst_info_dict_list[int(name.split('_')[1])-1] = inst_info_dict
+
+    results_list_7 = run_nuclei_type_stat(inst_info_dict_list, '/root/autodl-tmp/rubyyao/PycharmProjects/nuclei_ins_seg/data/CoNSeP/raw/consep/Test/Labels', 7)
+    results_list_4 = run_nuclei_type_stat(inst_info_dict_list, '/root/autodl-tmp/rubyyao/PycharmProjects/nuclei_ins_seg/data/CoNSeP/raw/consep/Test/Labels', 4)
+
+    # ret_metrics = {}
+    # ret_metrics['classification_F1'] = results_list
+
+    # if nan_to_num is not None:
+    #     ret_metrics = OrderedDict(
+    #         {metric: np.nan_to_num(metric_value, nan=nan_to_num)
+    #          for metric, metric_value in ret_metrics.items()})
+
+    return results_list_7, results_list_4
